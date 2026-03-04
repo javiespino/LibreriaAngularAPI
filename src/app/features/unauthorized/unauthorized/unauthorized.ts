40ea6c665237f92fbed5af45a13bb5fea7596765
javiespino <jespinot05@gmail.com>
2026-03-04 12:28:09 +0100

    Cambios en el acceso no autorizado

--- src/app/features/unauthorized/unauthorized/unauthorized.ts
+++ src/app/features/unauthorized/unauthorized/unauthorized.ts
@@ -4,11 +4,8 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-unauthorized',
   standalone: true,
-  template: `
-    <h2>Acceso no autorizado</h2>
-    <p>No tienes permisos para ver esta página.</p>
-    <button (click)="router.navigate(['/books'])">Volver</button>
-  `
+  templateUrl: './unauthorized.html',
+  styleUrls: ['./unauthorized.css']
 })
 export class UnauthorizedComponent {
   router = inject(Router);
